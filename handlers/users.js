1582f86c86b2055fd3acea3eb093c30ad3516d0b
Marcel Dall'Oglio <marcel.dalloglio.ext@infracommerce.com.br>
2023-02-01 15:04:59 -0300

    Teste package

--- handlers/users.js
+++ handlers/users.js
@@ -1,10 +1,14 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
+const to = require('to-case')
 
 exports.handler = async (event, context, callback) => {
   try {
     const users = await prisma.user.findMany({
       include: { profile: true }
+    })
+    users.map(user => {
+      user.teste = to.camel('what_the_heck')
     })
     return {
       statusCode: 200,
